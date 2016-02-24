637654461edc74c17d94abce0a724565966c7ee8
Ross Harper <harper05@mc-s083570.local>
2016-02-24 17:00:52 +0000

    Initial Commit

---
+++ gameoflife/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gameoflife
+//
+//  Created by Ross Harper on 24/02/2016.
+//  Copyright © 2016 BBC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
